a10eecc7ae7f3e8d91f2e6b866b39af56824785c
ttSchoolAcc <tim.trasmonte01@utrgv.edu>
2026-04-18 16:43:54 -0500

    Completed a bit more

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,10 +5,36 @@ public class GameManager : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI counterText;
     [SerializeField] int score;
+    [Space(10)]
+    [SerializeField] float timer;
+    [SerializeField] TextMeshProUGUI timerText;
+    [SerializeField] GameObject gameScreen;
+    [SerializeField] GameObject endScreen;
+    [SerializeField] TextMeshProUGUI resultText;
 
     public void PointIncrease(int value)
     {
         score += value;
         counterText.text = "Compressions Given: " + score;
+    }
+
+    void Update()
+    {
+        Timer();
+    }
+
+    public void Timer()
+    {
+        if(timer > 0)
+        {
+            timer -= Time.deltaTime;
+            timerText.text = "" + (int)timer;            
+        }
+        else        
+        {
+            resultText.text = score.ToString();
+            gameScreen.SetActive(false);
+            endScreen.SetActive(true);
+        }
     }
 }
